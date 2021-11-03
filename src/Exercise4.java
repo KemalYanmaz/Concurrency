52cee1feb3d4ba65924b5df805954a1f7c0db9f0
KemalYanmaz <47372602+KemalYanmaz@users.noreply.github.com>
2021-11-03 22:14:02 +0300

    Exercise5 locked and unlocked Lock around critical section

--- src/Exercise4.java
+++ src/Exercise4.java
@@ -1,16 +1,26 @@
-/**
+/*
  * Here a shared value using by more than one thread along reading,modifying and writing value of garlicCount.
  * We need control the accessing to garlicCount variable by one thread per time.
+ *
+ * Atomic Operation
+ * Execute as a single action, relative to other threads.
+ * Cannot be interrupted by other concurrent threads.
  */
+import java.util.concurrent.locks.*;
 
 class Shopper extends Thread{
 
     static int garlicCount = 0;
-
+    static Lock pencil = new ReentrantLock();
     @Override
     public void run() {
-        for(int i=0;i<10_000_000;i++){
+        for(int i=0;i<5;i++){
+            pencil.lock();
             garlicCount++;
+            pencil.unlock();
+            try {
+                Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace();}
+            System.out.println(Thread.currentThread().getName()+" is thinking");
         }
     }
 }
