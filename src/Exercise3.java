04ad7fda3d647f2f798ce2eb2b471242269af196
KemalYanmaz <47372602+KemalYanmaz@users.noreply.github.com>
2021-11-03 21:35:40 +0300

    Exercise3 added

---
+++ src/Exercise3.java
@@ -0,0 +1,36 @@
+import java.util.SortedMap;
+
+/**
+ * Daemon Threads
+ * Barron finishes cooking while Olivia cleans
+ */
+class KitchenCleaner extends Thread{
+    @Override
+    public void run() {
+        while (true){
+            System.out.println("Olivia cleaned the kitchen");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class Exercise3 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread olivia = new KitchenCleaner();
+        olivia.setDaemon(true);
+        olivia.start();
+
+        System.out.println("Barron is cooking...");
+        Thread.sleep(600);
+        System.out.println("Barron is cooking...");
+        Thread.sleep(600);
+        System.out.println("Barron is cooking...");
+        Thread.sleep(600);
+        System.out.println("Barron is done!");
+
+    }
+}
