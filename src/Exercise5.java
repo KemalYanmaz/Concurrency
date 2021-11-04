da677bc9c80f4c9cc3c99ee54aa321cd7209fe15
KemalYanmaz <47372602+KemalYanmaz@users.noreply.github.com>
2021-11-04 23:52:39 +0300

    Exercise6 added

--- src/Exercise5.java
+++ src/Exercise5.java
@@ -1,5 +1,5 @@
 /*
-Reente
+ReentrantLock
 
 
  */
---
+++ src/Exercise6.java
@@ -0,0 +1,61 @@
+/*
+tryLock method of Lock
+ */
+
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class itemCounters extends Thread{
+
+    private int itemsToAdd = 0; // items this shopper is waiting to add
+    private static int itemsOnNotepad = 0; // total items on shared notepad
+    private static Lock pencil = new ReentrantLock();
+
+    public itemCounters(String name){
+        setName(name);
+    }
+
+    @Override
+    public void run() {
+        while (itemsOnNotepad<20){
+            if(itemsToAdd>0){// add item(s) to shared notepad
+                try{
+                    pencil.lock();
+                    itemsOnNotepad++;
+                    System.out.println(this.getName() + " added "+ itemsToAdd+" item(s) to notepad.");
+                    itemsToAdd = 0;
+                    Thread.sleep(300);//time spend writing
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    pencil.unlock();
+                }
+            }else{ // look for other things to buy
+                try{
+                    Thread.sleep(100);
+                    itemsToAdd++;
+                    System.out.println(this.getName()+" found something to buy");
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
+
+public class Exercise6 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread barron = new itemCounters("Barron");
+        Thread olivia = new itemCounters("Olivia");
+        long start = System.currentTimeMillis();
+        barron.start();
+        olivia.start();
+        barron.join();
+        olivia.join();
+        long finish = System.currentTimeMillis();
+        System.out.println("Elapsed time: "+ (float) (finish-start)/1000+ " seconds");
+
+    }
+}
